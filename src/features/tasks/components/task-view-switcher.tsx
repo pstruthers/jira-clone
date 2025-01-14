9162fe1aa56b27666c4b50508658c1b1f535d77a
Paige <paige.struthers@gmail.com>
2025-01-14 13:48:44 -0600

    kanban data view

--- src/features/tasks/components/task-view-switcher.tsx
+++ src/features/tasks/components/task-view-switcher.tsx
@@ -13,6 +13,9 @@ import { useTaskFilters } from "../hooks/use-task-filters";
 import { DataTable } from "./data-table";
 import { columns } from "./columns";
 import { DataKanban } from "./data-kanban";
+import { useCallback } from "react";
+import { TaskStatus } from "../types";
+import { useBulkUpdateTasks } from "../api/use-bulk-update-tasks";
 
 export const TaskViewSwitcher = () => {
 	const workspaceId = useWorkspaceId();
@@ -29,6 +32,7 @@ export const TaskViewSwitcher = () => {
 		}] = useTaskFilters();
 
 	const { open } = useCreateTaskModal();
+	const { mutate: bulkUpdate } = useBulkUpdateTasks();
 
 	const { data: tasks, isLoading: isLoadingTasks } = useGetTasks({
 		workspaceId,
@@ -38,47 +42,62 @@ export const TaskViewSwitcher = () => {
 		dueDate,
 	});
 
+	const onKanbanChange = useCallback((
+		tasks: { $id: string; status: TaskStatus; position: number; }[]
+	) => {
+		bulkUpdate({
+			json: {tasks}
+		});
+	}, [bulkUpdate]);
+
 	return (
-		<Tabs defaultValue={view} onValueChange={setView} className="flex-1 w-full border rounded-lg">
-			<div className="h-full flex flex-col overflow-auto p-4">
-				<div className="flex flex-col gap-y-2 lg:flex-row justify-between items-center">
-					<TabsList className="w-full lg:w-auto">
-						<TabsTrigger className="h-8 w-full lg:w-auto" value="table">
-							Table
-						</TabsTrigger>
-						<TabsTrigger className="h-8 w-full lg:w-auto" value="kanban">
-							Kanban
-						</TabsTrigger>
-						<TabsTrigger className="h-8 w-full lg:w-auto" value="calendar">
-							Calendar
-						</TabsTrigger>
-					</TabsList>
-					<Button onClick={open} size="sm" className="w-full lg:w-auto">
-						<PlusIcon className="size-4 mr-2" />
-						New
-					</Button>
-				</div>
-				<DottedSeparator className="my-4" />
-				<DataFilters />
-				<DottedSeparator className="my-4" />
-				{isLoadingTasks ? (
-					<div className="w-full border rounded-lg h-[200px] flex flex-col items-center justify-center">
-						<Loader className="size-5 animate-spin text-muted-foreground" />
-					</div>
-				) : (
-				<>
-					<TabsContent value="table" className="mt-0">
-						<DataTable columns={columns} data={tasks?.documents ?? []} />
-					</TabsContent>
-					<TabsContent value="kanban" className="mt-0">
-						<DataKanban data={tasks?.documents ?? []} />
-					</TabsContent>
-					<TabsContent value="calendar" className="mt-0">
-						Data calendar
-					</TabsContent>
-		</>
-		)}
-			</div>
-		</Tabs>
-	);
+    <Tabs
+      defaultValue={view}
+      onValueChange={setView}
+      className="flex-1 w-full border rounded-lg"
+    >
+      <div className="h-full flex flex-col overflow-auto p-4">
+        <div className="flex flex-col gap-y-2 lg:flex-row justify-between items-center">
+          <TabsList className="w-full lg:w-auto">
+            <TabsTrigger className="h-8 w-full lg:w-auto" value="table">
+              Table
+            </TabsTrigger>
+            <TabsTrigger className="h-8 w-full lg:w-auto" value="kanban">
+              Kanban
+            </TabsTrigger>
+            <TabsTrigger className="h-8 w-full lg:w-auto" value="calendar">
+              Calendar
+            </TabsTrigger>
+          </TabsList>
+          <Button onClick={open} size="sm" className="w-full lg:w-auto">
+            <PlusIcon className="size-4 mr-2" />
+            New
+          </Button>
+        </div>
+        <DottedSeparator className="my-4" />
+        <DataFilters />
+        <DottedSeparator className="my-4" />
+        {isLoadingTasks ? (
+          <div className="w-full border rounded-lg h-[200px] flex flex-col items-center justify-center">
+            <Loader className="size-5 animate-spin text-muted-foreground" />
+          </div>
+        ) : (
+          <>
+            <TabsContent value="table" className="mt-0">
+              <DataTable columns={columns} data={tasks?.documents ?? []} />
+            </TabsContent>
+            <TabsContent value="kanban" className="mt-0">
+              <DataKanban
+                data={tasks?.documents ?? []}
+                onChange={onKanbanChange}
+              />
+            </TabsContent>
+            <TabsContent value="calendar" className="mt-0">
+              Data calendar
+            </TabsContent>
+          </>
+        )}
+      </div>
+    </Tabs>
+  );
 };
